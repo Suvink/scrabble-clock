6826d0ba54be31f98a041d375371b4e0cd23662f
Suvin Nimnaka <hello@suvin.me>
2023-05-10 12:20:33 +0530

    Add UOC Scrabble description

--- screens/AboutScreen/index.js
+++ screens/AboutScreen/index.js
@@ -12,10 +12,13 @@ const AboutScreen = () => {
             <Image source={SOC} style={{ width: 300, height: 200 }} />
             <View style={styles.textContainer}>
                 <Text category='h6' style={styles.aboutText}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    The University of Colombo (UOC) is home to an avid Scrabble community, 
+                    which comprises a plethora of skilled players. Over the years, 
+                    UoC has dominated the University Scrabble arena, clinching numerous titles and 
+                    accolades in various competitions. With a reputation for excellence and a 
+                    robust player base, the University of Colombo is undoubtedly a hub of 
+                    Scrabble expertise, where enthusiastic undergraduates of the game can engage in 
+                    thrilling matches and hone their skills.
                 </Text>
             </View>
         </Layout>
