c0a7489b9030da9cec2337700ff29ad3c56f5053
Suvin Nimnaka <hello@suvin.me>
2026-03-08 20:37:48 +0530

    Add release info

--- screens/AboutScreen/index.js
+++ screens/AboutScreen/index.js
@@ -29,7 +29,7 @@ const AboutScreen = () => {
                 <Image source={SOCLogo} style={{ width: 300, height: 200 }} />
             )}
             <Text category="h6" style={styles.aboutScreenSubtitleText}>
-                Version: 3.1.0
+                Version: 3.2.0
             </Text>
             <View style={styles.textContainer}>
                 {isIos ? (
